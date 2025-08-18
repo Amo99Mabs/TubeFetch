8c62d136ec7fde78c530bfd763b8d214205f00c4
Amo99Mabs <174100269+Amo99Mabs@users.noreply.github.com>
2025-08-18 19:57:24 +0200

    Created basic YouTube downloader GUI app using pytube and customtkinter
    - Implemented YouTube video downloader with pytube
    - Created GUI using customtkinter
    - Added input field for YouTube link
    - Displayed video title once link is processed
    - Implemented highest resolution video download
    - Added status label to show download progress (success or error)
    - Included "Download" button to trigger download process
    - Set up app window with custom size and theme
    - Handled basic exception for failed downloads

---
+++ convert.py
@@ -0,0 +1,47 @@
+import tkinter
+import customtkinter
+from pytube import YouTube
+
+def startDownload():
+    try:
+        ytLink = link.get()
+        ytObject = YouTube(ytLink)
+        video = ytObject.streams.get_highest_resolution()
+
+
+        title.configure(text=ytObject.title, text_color="white")
+        finishLabel.configure(text="")
+        video.download()
+        finishLabel.configure(text="Downloaded!")
+    except:
+        finishLabel.configure(text="Download Error", text_color="red")
+
+#System Settings
+customtkinter.set_appearance_mode("System")
+customtkinter.set_default_color_theme("blue")
+
+#Our app frame
+app = customtkinter.CTk()
+app.geometry("720x480")
+app.title("TubeFetch")
+
+#Adding UI Elements
+title = customtkinter.CTkLabel(app, text="Insert a youtube link")
+title.pack(padx=10, pady=10)
+
+# Link input
+url_var = tkinter.StringVar()
+link = customtkinter.CTkEntry(app, width=350, height=40, textvariable=url_var)
+link.pack()
+
+# Finished Downloading
+finishLabel = customtkinter.CTkLabel(app, text="")
+finishLabel.pack()
+
+#Download Button
+download = customtkinter.CTkButton(app, text="Download", command=startDownload)
+download.pack(padx=10, pady=10)
+
+# Run app
+app.mainloop()
+
